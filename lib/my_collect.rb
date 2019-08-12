1010a660113143531e87036fec0737aa73a5eeaf
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-12 22:12:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
-# def my_collect(array)
-# collection = []
-#   i=0
-#   while i < array.length do 
-#   collection << yield(array[i])
-#   i+=1
-# end
+def my_collect(array)
+collection = []
+  i=0
+  while i < array.length do 
+  collection << yield(array[i])
+  i+=1
+end
 
-# end
+end
 
