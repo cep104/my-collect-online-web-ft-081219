4aff5cb1e26a5da620a368d9ce30beb9a545832d
Candice Peters <51180851+cep104@users.noreply.github.com>
2020-11-09 16:01:40 -0600

    Update my_collect.rb
    notes

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
-def my_collect(array)
-collection = []
-  i=0
-  while i < array.length do 
-  collection << yield(array[i])
-  i+=1
+def my_collect(array) 
+collection = [] #make an empty array to store everything in
+  i=0 #set index at 0
+  while i < array.length do #while the index number is less than the length 
+  collection << yield(array[i]) #insert the array into the collection
+  i+=1 #add one to indexeach time 
 end
-collection
+collection #return the new collection
 end
 
